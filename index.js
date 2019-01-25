46f12fc2eb9c9ce31f1579705164f14fcec82023
Jamal Amar Bahena <jamal@Jamals-MacBook-Pro.local>
2019-01-25 15:20:24 -0600

    created app on index.js

--- index.js
+++ index.js
@@ -3,5 +3,22 @@ let express = require('express')
 let mongoose = require('mongoose')
 let app = express()
 let movieRouter = require('./movies/controller')
+let bodyParser = require('body-parser')
+
+mongoose.connect('mongodb://localhost:27017/library', { useNewUrlParser: true })
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+let errorMiddleWare = function(error, req, res, next) {
+	res.status(500).json({error: true})
+}
+
+app.use(errorMiddleWare)
+app.use('/movies', movieRouter)
 
 
+const PORT = 9000
+
+app.listen(PORT)
+console.log("Listening on port: ", PORT)
